6baa7359eb399f50d74c2cd4bf40d2fa6248d1ec
Noah Colangelo <ncolangelo@localhost.localdomain>
2022-06-30 16:00:01 -0400

    Changed the system and server uptime to be in seconds and can now properly log the local date/time and store it in a date object

--- tcm_app/src/snmpFunc.js
+++ tcm_app/src/snmpFunc.js
@@ -1,3 +1,5 @@
+//import Buffer from 'node:buffer';
+
 var snmp = require ("net-snmp");
 
 //var session = snmp.createSession ("127.0.0.1", "idv90we3rnov90wer");
@@ -5,10 +7,19 @@ var session = snmp.createSession ("127.0.0.1", "209ijvfwer0df92jd");
 
 session.version = snmp.Version2c;
 
-//           sysUpTimeInstance    uptime of computer       Local date and time?
-var oids = ["1.3.6.1.2.1.1.3.0", "1.3.6.1.2.1.25.1.1.0", "1.3.6.1.2.1.25.1.2.0"];
+
+var oids = ["1.3.6.1.2.1.1.3.0",// server upTime
+            "1.3.6.1.2.1.25.1.1.0",// system upTime
+            "1.3.6.1.2.1.25.1.2.0",// Local Date and Time
+            "1.3.6.1.2.1.25.2.3.1.5.101",// total available memory
+            "1.3.6.1.2.1.25.2.3.1.6.101",//current memory usage
+            "1.3.6.1.2.1.25.4.2.1.3"];// CPU usage%
 
 let sysUpTimeSeconds = 0;
+let compUpTimeSeconds = 0;
+
+let buf1;
+let localDateTime;
 
 session.get (oids, function(error, varbinds){
     if(error)
@@ -25,13 +36,43 @@ session.get (oids, function(error, varbinds){
             }
             else
             {
-                //if(i === 0)
-                //{
-                //    console.log(varbinds[i].oid + " = " + (varbinds[i].value / 360000));
-                //    sysUpTimeSeconds = varbinds[i].value /100;
-                //}
-                //else
-                    console.log(varbinds[i].oid + " = " + varbinds[i].value);
+                if(i === 0)//for getting the server upTime
+                {
+                    console.log(varbinds[i].oid + " = " + (varbinds[i].value / 100));
+                    sysUpTimeSeconds = varbinds[i].value /100;
+                }
+                else if (i ===1)//for getting the upTime of when the OS turned on
+                {
+                    console.log(varbinds[i].oid + " = " + varbinds[i].value / 100);
+                    compUpTimeSeconds = varbinds[i].value/100;
+                }
+                else if (i === 2)//for getting the local date and time 
+                {
+                    buf1 = Buffer.from(varbinds[i].value);
+
+                    localDateTime = new Date(
+                        buf1.readUint16BE(0),
+                        buf1.readUint8(2) -1,
+                        buf1.readUint8(3),
+                        buf1.readUint8(4),
+                        buf1.readUint8(5),
+                        buf1.readUint8(6)
+
+                    )
+
+                    console.log(localDateTime.toString());
+
+                    //localDateTime = '';
+                    //localDateTime = buf1.readUint16BE(0) + ' ';
+
+                    //for (let i = 2; i < buf1.byteLength; i++)
+                    //{
+                    //    localDateTime += buf1.readUint8(i) + ' ';
+                    //}
+                    //sconsole.log(localDateTime);
+                }
+                else
+                console.log(varbinds[i].oid + " = " + (varbinds[i].value));
             }
         }
     }
